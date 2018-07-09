c41f366b70050d87a6e9bd44fa0a7278d1d5c652
jp1527 <jp1527@126.com>
2018-07-09 13:39:10 +0800

    uiuiu

--- ConsoleGithub/Program.cs
+++ ConsoleGithub/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleGithub
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("77g76ood ");
+            Console.WriteLine("77g76odfgod ");
         }
     }
 }
